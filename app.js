2e3a80f2a2bd071901b8a2caf7c46d582b531e60
Ricardo Xavier <rxc.projects@gmail.com>
2024-06-18 09:56:50 -0300

    configurando banco de dados 'railway'.

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ import cors from 'cors'
 import getSubscribe from './routes/getSubscribe.js'
 
 const app = express()
-const port = 3333
+const port = process.env.PORT || 3333
 
 app.use(express.json())
 app.use(cors({ origin: '*' }));
--- db.js
+++ db.js
@@ -1,19 +1,22 @@
 import { Sequelize } from "sequelize"
 
-const database = 'df_database'
-const username = 'root'
-const password = 'admin'
-
+const host = process.env.MYSQLHOST
+const database = process.env.MYSQLDATABASE
+const username = process.env.MYSQLUSER
+const password = process.env.MYSQLPASSWORD
+const dbPort = process.env.MYSQLPORT
 
 const sequelize = new Sequelize(database, username, password, {
-  host: 'localhost',
-  dialect: 'mysql'
+  host: host,
+  dialect: 'mysql',
+  port: dbPort
 })
 
 try {
   await sequelize.authenticate();
   console.log('Connection has been established successfully.');
 } catch (error) {
+  console.log(database, password)
   console.error('Unable to connect to the database:', error);
 }
 
